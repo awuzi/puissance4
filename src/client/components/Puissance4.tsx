bd21dd7044b3fc9c8161afd2a88b6044b97c3c3f
Diyar <diyar@digiplace.fr>
2022-09-07 11:53:44 +0200

    animation

--- src/client/components/Puissance4.tsx
+++ src/client/components/Puissance4.tsx
@@ -9,25 +9,38 @@ const Puissance4 = ({gameData}: Puissance4Props) => {
     const canvasRef = useRef(null)
     const [click, setClick] = useState(0);
 
-    function addJeton(player = 1, ligne = 2, colonne = 0) {
+    function drawJeton(player = 1, ligne = 4, colonne = 0) {
         const canvas = canvasRef.current
         // @ts-ignore
         const ctx = canvas.getContext('2d')
-        ctx.beginPath()
         ctx.fillStyle = (player === 1) ? "#c82124" : "#1e90ff";
-        const x = ((colonne) * 45.47) + 46;
-        const y = ((ligne) * 40.2) + 40.2;
-        ctx.arc(x, y, 35, 0, 2 * Math.PI)
+        const x = colonne == 0 ? 50 : (colonne * 90) + 50;
+        const y = ligne == 0 ? 41 : (ligne * 80) + 41;
+
+        for (let i = 0; i < y; i++) {
+            setTimeout(function() {
+                animate(ctx, x, i);
+            }, 1000 / 30);
+        }
+    }
+
+    function animate(ctx: any, x: number, i: number) {
+        let bg = new Image();
+        bg.src = "/assets/grille.png";
+        ctx.clearRect(0, 0, 640, 480);
+        ctx.drawImage(bg, 0, 0, 640, 480);
+        ctx.beginPath();
+        ctx.arc(x, i, 35, 0, 2 * Math.PI)
         ctx.fill()
+        console.log("i work");
     }
 
     useEffect(() => {
-        addJeton()
+        drawJeton()
     }, [click]);
 
     return (
         <>
-            <button onClick={() => setClick(click + 1)}>Click</button>
             <canvas ref={canvasRef} width="640" height="480" className="bg-amber-50" style={{ height: '480px', width: '640px', backgroundImage: "url('./assets/grille.png')" }}></canvas>
         </>
     )
