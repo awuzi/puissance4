4b7aa212a3bdea97c9b4965e3f6b4efe419eb91c
Diyar <diyar@digiplace.fr>
2022-09-07 14:39:15 +0200

    animation

--- src/client/components/Puissance4.tsx
+++ src/client/components/Puissance4.tsx
@@ -1,13 +1,27 @@
 import React, {useEffect, useRef, useState} from 'react';
+import {CellState, GridState} from "../../domain/types";
 
 interface Puissance4Props {
-    gameData: number[][]
+    gameData: GridState
 }
 
 const Puissance4 = ({gameData}: Puissance4Props) => {
 
     const canvasRef = useRef(null)
     const grilleCoords = [100, 190, 280, 370, 460, 550, 640];
+
+    useEffect(() => {
+        defaultState();
+    }, []);
+    function defaultState() {
+        gameData.forEach((row: CellState[], rowIndex: number) => {
+           row.forEach((col: CellState, colIndex: number) => {
+               if (col !== '_') {
+                   dropJeton(col, rowIndex, colIndex, true)
+               }
+           });
+        });
+    }
 
     function clickGrille(click: React.MouseEvent) {
         const canvas = canvasRef.current
@@ -18,28 +32,32 @@ const Puissance4 = ({gameData}: Puissance4Props) => {
 
         for (const [index, coord] of grilleCoords.entries()) {
             if (x < coord) {
-                drawJeton(1, 5, index);
+                dropJeton('R', 5, index);
                 break;
             }
         }
     }
 
-    function drawJeton(player = 1, ligne = 2, colonne = 0) {
+    function dropJeton(player = 'R', ligne = 2, colonne = 0, instant = false) {
         const canvas = canvasRef.current
         // @ts-ignore
         const ctx = canvas.getContext('2d')
-        ctx.fillStyle = (player === 1) ? "#c82124" : "#1e90ff";
+        ctx.fillStyle = (player === 'R') ? "#c82124" : "#FFFF00";
         const x = colonne == 0 ? 50 : (colonne * 90) + 50;
         const y = ligne == 0 ? 41 : (ligne * 80) + 41;
-        let i = 0;
-        const anim = setInterval(function() {
-            i++;
-            animate(ctx, x, i);
-            if (i >= y) clearInterval(anim);
-        }, 1);
+        if (instant) {
+            drawJeton(ctx, x, y);
+        }else {
+            let i = 0;
+            const anim = setInterval(function () {
+                i++;
+                drawJeton(ctx, x, i);
+                if (i >= y) clearInterval(anim);
+            }, 1);
+        }
     }
 
-    function animate(ctx: any, x: number, i: number) {
+    function drawJeton(ctx: any, x: number, i: number) {
         ctx.beginPath();
         ctx.clearRect(x-35, i-37, 70, 35);
         ctx.arc(x, i, 35, 0, 2 * Math.PI)
