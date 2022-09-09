eabd7eb571fd25aa8a29b4b995ee730139d0ebe0
Diyar <diyar@digiplace.fr>
2022-09-09 22:07:39 +0200

    partie terminee

--- src/client/pages/Game.tsx
+++ src/client/pages/Game.tsx
@@ -1,14 +1,10 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { GameAction, GameState } from "../../domain/types";
-import Logo from "../components/Logo";
+import React, { useContext } from 'react';
 import Puissance4 from "../components/Puissance4";
-import Link from "../components/Link";
 import { GameContext, socket } from "../context";
 
 export const Game = () => {
 
   const { context, setContext } = useContext(GameContext);
-  const [playing, setPLaying] = useState(true);
 
   return (
     <>
@@ -16,19 +12,6 @@ export const Game = () => {
       <div style={{ display: 'flex', flexDirection: 'row', height: '100%' }}>
 
         <Puissance4/>
-        {!playing ?
-          <div className="float-left bg-amber-50 bg-opacity-90 absolute text-center" style={{ height: "480px", width: "640px" }}>
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold">
-              La partie est terminée
-              <br/>
-              <Link route="/">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5">
-                  Nouvelle partie
-                </button>
-              </Link>
-            </div>
-          </div>
-          : ''}
         <pre>{JSON.stringify(context, null, 2)}</pre>
       </div>
       {/*</div>*/}
