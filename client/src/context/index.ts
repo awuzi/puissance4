69edb7b89c7fd466ec7f68a4a2229cfdc7a5c549
Diyar <diyar@digiplace.fr>
2022-09-12 01:00:55 +0200

    https enabled

--- client/src/context/index.ts
+++ client/src/context/index.ts
@@ -2,7 +2,7 @@ import React from "react";
 import { io } from "socket.io-client";
 import { GameState } from "../domain/types";
 
-export const socket = io("ws://server.puissance.diyar.dev/");
+export const socket = io("wss://server.puissance.diyar.dev/");
 export const GameContext = React.createContext({
   context: {} as GameState,
   setContext: (v: GameState | any): void => {}
