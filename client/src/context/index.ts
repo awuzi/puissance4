0dcf792ca566e752372992d53b0c6803d0d24b19
Diyar <diyar@digiplace.fr>
2022-09-12 00:37:35 +0200

    fix

--- client/src/context/index.ts
+++ client/src/context/index.ts
@@ -2,7 +2,7 @@ import React from "react";
 import { io } from "socket.io-client";
 import { GameState } from "../domain/types";
 
-export const socket = io("ws://localhost:8080/");
+export const socket = io("ws://164.92.239.154:8080/");
 export const GameContext = React.createContext({
   context: {} as GameState,
   setContext: (v: GameState | any): void => {}
